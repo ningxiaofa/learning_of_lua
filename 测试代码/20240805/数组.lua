1617d4a01423da9701d4a85028c1c531b658188f
ningxinfa <ningxinfa@wudi360.com>
2025-07-28 09:56:09 +0800

    20250726-before

---
+++ 测试代码/20240805/数组.lua
@@ -0,0 +1,12 @@
+local msg_id = 1
+print(msg_id)
+print(#msg_id)
+
+
+-- ➜  learning_of_lua git:(main) ✗ lua 测试代码/20240805/数组.lua
+-- 1
+-- lua: 测试代码/20240805/数组.lua:3: attempt to get length of a number value (local 'msg_id')
+-- stack traceback:
+--         测试代码/20240805/数组.lua:3: in main chunk
+--         [C]: in ?
+-- ➜  learning_of_lua git:(main) ✗ 
