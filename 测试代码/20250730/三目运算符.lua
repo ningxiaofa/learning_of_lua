aab46d9b20ead264c561c60d842c891b87014361
nxf <nxf@test.com>
2025-08-13 22:38:47 +0800

    20250813-1

--- 测试代码/20250730/三目运算符.lua
+++ 测试代码/20250730/三目运算符.lua
@@ -1,4 +1,4 @@
--- local type1 = 1 < 2 ? "less" : "normal" -- 兜底结算类型 -- 不支持
+-- local type1 = 1 < 2 ? "less" : "normal" -- 兜底结算类型 -- 不支持三目运算符
 -- print(type1)
 
 -- 兜底结算类型
@@ -7,3 +7,20 @@ if (1 < 2) then
     type = "less"
 end
 print(type)
+
+print("-----------------------------")
+
+-- 间接实现
+a,b,c=1,2,3
+a=false;
+d=(a and b) or c;
+print(d);--3
+print("\n");
+
+A,B,C=1,2,3;
+B=false;
+D=(A and B)or C;
+print(D);--3 因为当B=false时,继续走C
+
+E=((A and{B}) or {C})[1];--这里才是一个合格的三目运算 {B},{C}都是table[索引数组],一定为真,返回第一个元素即可
+print(E);
